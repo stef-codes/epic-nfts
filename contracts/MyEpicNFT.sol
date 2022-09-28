0e4dd3f33fa82c8ef2f07e42ae031e58c3367586
Stefanie <stefcodes@gmail.com>
2022-09-28 00:19:17 -0700

    create smart contract

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("Another Contract is here. Don't Call it a comeback!");
+    }
+}
